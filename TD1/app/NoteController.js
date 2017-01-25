b8a6a7be95df286b85863dd57467f0b1f69f9187
quentindurpoix <durpoixquentin@gmail.com>
2017-01-25 16:49:01 +0100

    Commit

--- TD1/app/NoteController.js
+++ TD1/app/NoteController.js
@@ -1,16 +1,33 @@
-var myApp = angular.module('TD1');
+var myApp = angular.module('TD1',["$cookies",function($cookies){
+    // Retrieving a cookie
+    var favoriteCookie = $cookies.get('myFavorite');
+    // Setting a cookie
+    $cookies.put('myFavorite', 'oatmeal');
+}]);
 
 myApp.controller('controllerTD1', function(){
-	
+
+    this.info="Note modifiée";
 	this.caract=100;
+    this.message = "";
 	var self = this;
+
 
 	this.clear = function(){
 		self.message = "";
+        self.info="Note modifiée";
 	};
 	
 	this.count = function(){
+
 		self.caract = 100 - self.message.length ;
 	};
+
+    this.save = function(){
+        if(self.message.length>0){
+            self.info = "Note sauvegardée" ;
+        }
+
+    };
 	
 });
