6c132faaa91fbe51d5620ad865ab969c9fd52bb7
quentindurpoix <durpoixquentin@gmail.com>
2017-02-07 16:18:01 +0100

    milieu EXO2 + .gitingnore

--- TD2_EX2/app/servicesController.js
+++ TD2_EX2/app/servicesController.js
@@ -5,7 +5,9 @@
 var myApp = angular.module('TD2');
 
 myApp.controller('controllerTD2',['$http', function($http){
-
+    var self = this;
+    self.results = [];
+    self.contacts = [];
     $http.get("./app/contacts.json").then(
         function (response) {
             self.contacts = response.data;
@@ -13,11 +15,28 @@ myApp.controller('controllerTD2',['$http', function($http){
 
      this.edit=false;
 
-     var self = this;
+
+    this.cancelOne = function(){
+        self.results.forEach(function(item, index, object) {
+            self.contacts.push(item);
+        });
+        self.contacts.forEach(function(item, index, object) {
+            if (item.deleted == true) {
+                item.deleted=false;
+            }
+        });
+        self.results=[];
+    };
 
     this.toAdd = function(){
         self.edit ="add";
     };
+
+    this.deleter = function(contact){
+        self.results.push(contact);
+        contact.deleted = true;
+    };
+
 
 }]);
 
@@ -30,5 +49,18 @@ myApp.directive('contactElem', function() {
 myApp.directive('frmContactElem', function() {
     return {
         templateUrl: './template/frmContacts.html'
+    };
+});
+
+myApp.filter('NotDeletedFilter', function(){
+    return function(input){
+
+        input.forEach(function(item, index, object) {
+                if (item.deleted == true) {
+                    object.splice(index, 1);
+                }
+        });
+
+    return input;
     };
 });
